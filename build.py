055d2f4fc9e6c39e9da7a8fbcec7cff67d376b98
Randall Frank <frogboots.000@gmail.com>
2025-09-29 14:57:21 -0400

    Retool source
    - Move many symbols to common file
    - Expand the redraw area - prep for "shadows"
    - New memory layout
    - Many more RAM locations documented

--- build.py
+++ build.py
@@ -5,8 +5,8 @@ assembler = ".\\Merlin32_v1.2_b2\\Windows\\Merlin32.exe"
 assembler_libdir = ".\\Merlin32_v1.2_b2\\Library\\"
 
 files = {
-    "BOTTOMLINE.S": 0x0C80,
-    "DRAWTAB.S": 0x9600,
+    # "BOTTOMLINE.S": 0x0C80,
+    "DRAWTAB.S": 0x4E00,  # 0x9600,
     "GAME1.S": 0x7400,
     "GAME2.S": 0x7A00,
     "IO2.S": 0x9000,
@@ -43,17 +43,17 @@ bins = {
     "bin/SOUNDS#068900": 0x3900,     # reloc=0x8900
     "bin/LOOKTBLS#060C00": 0x2E00,   # reloc=0x0E00
     "bin/DECOMP_IMG.BIN": 0x4000,
-    "bin/diaflw_splash.fgr": 0x4100,
-    "bin/diaflw_play.fgr": 0x4A00,
+    "bin/diaflw_splash.fgr": 0x4100, 
+    "bin/diaflw_play.fgr": 0x4A00,   
     "bin/HELP.BIN": 0x7000,          # this is overwritten after moved to text
-    "bin/BOTTOMLINE.BIN": 0x2C80,    # reloc=0x0C80,
-    "bin/DRAWTAB.BIN": 0x2700,       # reloc=0x9600,
+    # "bin/BOTTOMLINE.BIN": 0x2C80,    # reloc=0x0C80,
     "bin/GAME1.BIN": 0x6400,         # reloc=0x7400,
     "bin/GAME2.BIN": 0x6A00,         # reloc=0x7A00,
     "bin/IO2.BIN": 0x2100,           # reloc=0x9000,
     "bin/OPENING2.BIN": 0x3000,      # reloc=0x8000,
     "bin/PAC.BIN": 0x2800,           # reloc=0x0800,
     "bin/WEAPONS.BIN": 0x6200,       # reloc=0x7200,
+    "bin/DRAWTAB.BIN": 0x4E00,       # reloc=0x4E00,
 }
 '''
     "PRE.S": 0x9700,   # prefix support for editors
@@ -62,7 +62,7 @@ bins = {
 
 # 2000 - LOADER.S
 #     Reads from 2800-3000 and stores to 0800-1000
-#     Reads from 2100-2800 and stores to 9000-9700
+#     Reads from 2100-2800 and stores to 9000-9800
 #     Reads from 3000-4000 and stores to 8000-9000
 #     Reads from 6000-7000 and stores to 7000-8000
 #     Paint the text page from 7000-7400
@@ -74,14 +74,18 @@ with open('DIAFLW_SYSTEM_orig.bin', 'rb') as fp:
     data = bytearray(fp.read())
 
 for name, addr in bins.items():
+    print(f"Loading {name} at {addr:04X}")
     with open(name, "rb") as f:
         local = bytearray(f.read())
     offset = addr - 0x2000
     length = len(local)
     data[offset:offset+length] = local
 
-with open("bin/DIAFLW.SYSTEM#ff2000", "wb") as fp:
+outname = "bin/DIAFLW.SYSTEM#ff2000"
+with open(outname, "wb") as fp:
     fp.write(data)
+    
+print(f"Wrote: {outname}")
     
 
 # Build 'READ.DOCS,TSYS' from 'bin/PRG.BIN'
